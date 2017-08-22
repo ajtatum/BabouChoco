58e72057461498c420e8f91dcc6aebdb4feb5602
AJ Tatum <andrew.j.tatum@gmail.com>
2017-08-21 23:00:07 -0400

    Inital commit to get chocolatey packages

--- BabouChoco/MainWindow.xaml.cs
+++ BabouChoco/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management.Automation;
+using System.Threading;
+
 
 namespace BabouChoco
 {
@@ -23,6 +27,72 @@ namespace BabouChoco
         public MainWindow()
         {
             InitializeComponent();
+
+            TxtChocoPackages.Text = string.Join(Environment.NewLine, GetInstalledPackages());
+        }
+
+        public List<string> GetInstalledPackages()
+        {
+            var psOutput = new StringBuilder();
+            var chocoPackages = new List<string>();
+
+            using (var ps = PowerShell.Create())
+            {
+                ps.AddScript("choco list -lo");
+
+                // prepare a new collection to store output stream objects
+                var outputCollection = new PSDataCollection<PSObject>();
+                outputCollection.DataAdded += outputCollection_DataAdded;
+
+                // the streams (Error, Debug, Progress, etc) are available on the PowerShell instance.
+                // we can review them during or after execution.
+                // we can also be notified when a new item is written to the stream (like this):
+                ps.Streams.Error.DataAdded += Error_DataAdded;
+
+
+                var result = ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+                while (result.IsCompleted == false)
+                {
+                    Console.WriteLine(@"Waiting for pipeline to finish...");
+                    Thread.Sleep(1000);
+                }
+                Console.WriteLine($@"Execution has stopped. The pipeline state: {ps.InvocationStateInfo.State}");
+
+                foreach (PSObject outputItem in outputCollection)
+                {
+                    //TODO: handle/process the output items if required
+                    var outputString = outputItem.BaseObject.ToString();
+                    Console.WriteLine(outputString);
+                    psOutput.AppendLine(outputString);
+                    chocoPackages.Add(outputString);
+                }
+            }
+
+            chocoPackages.RemoveAt(chocoPackages.Count-1);
+
+            return chocoPackages;
+        }
+
+        /// <summary>
+        /// Event handler for when data is added to the output stream.
+        /// </summary>
+        /// <param name="sender">Contains the complete PSDataCollection of all output items.</param>
+        /// <param name="e">Contains the index ID of the added collection item and the ID of the PowerShell instance this event belongs to.</param>
+        private void outputCollection_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            // do something when an object is written to the output stream
+            Console.WriteLine("Object added to output.");
+        }
+
+        /// <summary>
+        /// Event handler for when Data is added to the Error stream.
+        /// </summary>
+        /// <param name="sender">Contains the complete PSDataCollection of all error output items.</param>
+        /// <param name="e">Contains the index ID of the added collection item and the ID of the PowerShell instance this event belongs to.</param>
+        private void Error_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            // do something when an error is written to the error stream
+            Console.WriteLine("An error was written to the Error stream!");
         }
     }
 }
