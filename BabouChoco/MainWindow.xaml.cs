7e95db0c3b8fe6291b06a06d0025ae233841fab3
AJ Tatum <andrew.j.tatum@gmail.com>
2017-08-21 23:44:34 -0400

    Added DataGrid to display items installed and version.

--- BabouChoco/MainWindow.xaml.cs
+++ BabouChoco/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Management.Automation;
 using System.Threading;
+using BabouChoco.Models;
 
 
 namespace BabouChoco
@@ -33,8 +34,9 @@ namespace BabouChoco
 
         public List<string> GetInstalledPackages()
         {
-            var psOutput = new StringBuilder();
-            var chocoPackages = new List<string>();
+            //var chocoPackages = new List<string>();
+
+            var chocoPackages = new List<ChocoPackage>();
 
             using (var ps = PowerShell.Create())
             {
@@ -42,13 +44,12 @@ namespace BabouChoco
 
                 // prepare a new collection to store output stream objects
                 var outputCollection = new PSDataCollection<PSObject>();
-                outputCollection.DataAdded += outputCollection_DataAdded;
+                outputCollection.DataAdded += OutputCollection_DataAdded;
 
                 // the streams (Error, Debug, Progress, etc) are available on the PowerShell instance.
                 // we can review them during or after execution.
                 // we can also be notified when a new item is written to the stream (like this):
                 ps.Streams.Error.DataAdded += Error_DataAdded;
-
 
                 var result = ps.BeginInvoke<PSObject, PSObject>(null, outputCollection);
                 while (result.IsCompleted == false)
@@ -58,19 +59,35 @@ namespace BabouChoco
                 }
                 Console.WriteLine($@"Execution has stopped. The pipeline state: {ps.InvocationStateInfo.State}");
 
-                foreach (PSObject outputItem in outputCollection)
+                foreach (var outputItem in outputCollection)
                 {
-                    //TODO: handle/process the output items if required
                     var outputString = outputItem.BaseObject.ToString();
-                    Console.WriteLine(outputString);
-                    psOutput.AppendLine(outputString);
-                    chocoPackages.Add(outputString);
+
+                    if (!outputString.Contains("packages installed."))
+                    {
+                        Console.WriteLine($@"{outputString} - adding to list");
+
+                        var outputArray = outputString.Split(' ');
+
+                        var package = new ChocoPackage()
+                        {
+                            Name = outputArray[0].Trim(),
+                            InstalledVersion = outputArray[1].Trim()
+                        };
+
+                        chocoPackages.Add(package);
+                    }
+                    else
+                    {
+                        Console.WriteLine($@"{outputString} - not adding to list");
+                    }
                 }
             }
 
-            chocoPackages.RemoveAt(chocoPackages.Count-1);
+            DgInstalledChocoPackages.AutoGenerateColumns = true;
+            DgInstalledChocoPackages.ItemsSource = chocoPackages;
 
-            return chocoPackages;
+            return chocoPackages.Select(x => x.Name).ToList();
         }
 
         /// <summary>
@@ -78,10 +95,10 @@ namespace BabouChoco
         /// </summary>
         /// <param name="sender">Contains the complete PSDataCollection of all output items.</param>
         /// <param name="e">Contains the index ID of the added collection item and the ID of the PowerShell instance this event belongs to.</param>
-        private void outputCollection_DataAdded(object sender, DataAddedEventArgs e)
+        private void OutputCollection_DataAdded(object sender, DataAddedEventArgs e)
         {
             // do something when an object is written to the output stream
-            Console.WriteLine("Object added to output.");
+            Console.WriteLine(@"Object added to output.");
         }
 
         /// <summary>
@@ -92,7 +109,7 @@ namespace BabouChoco
         private void Error_DataAdded(object sender, DataAddedEventArgs e)
         {
             // do something when an error is written to the error stream
-            Console.WriteLine("An error was written to the Error stream!");
+            Console.WriteLine(@"An error was written to the Error stream!");
         }
     }
 }
---
+++ BabouChoco/Models/ChocoPackage.cs
@@ -0,0 +1,8 @@
+﻿namespace BabouChoco.Models
+{
+    public class ChocoPackage
+    {
+        public string Name { get; set; }
+        public string InstalledVersion { get; set; }
+    }
+}
